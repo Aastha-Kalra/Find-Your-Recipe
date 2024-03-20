070b90141356254df3b83ff10bf7cda569076b89
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-20 20:24:56 +0530

    update RecipeIndex

--- src/Meal/RecipeIndex.jsx
+++ src/Meal/RecipeIndex.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RecipeIndex = ({alphaIndex}) => {
     const alpha = ["A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q" , "R", "S", "T", "U", "V", "W" ,"X", "Y", "Z"]
   return (
-    <div className='flex justify-center items-center gap-3 text-3xl'>
+    <div className='grid grid-cols-10 gap-3  md:text-3xl'>
         {
             alpha.map((item, i)=>{
                 return(
